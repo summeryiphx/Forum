337e1ff0465bf54362ed658d2b13c7ec2ca415c2
summeryip <835726864@qq.com>
2020-02-27 14:42:25 +0800

    init repo

---
+++ src/test/java/com/dgut/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgut.community;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
